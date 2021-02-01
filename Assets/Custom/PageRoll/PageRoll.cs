af1d04804571e2bb61743ab71124af13eea8b02e
Chu Yong <415161584@qq.com>
2021-02-01 17:54:39 +0800

    添加项目文件。

---
+++ Assets/Custom/PageRoll/PageRoll.cs
@@ -0,0 +1,288 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageRoll : MonoBehaviour
+{
+    public int pageCount
+    {
+        get
+        {
+            return Mathf.FloorToInt(pageTexs.Length / 2);
+        }
+    }
+    public int pageResCount
+    {
+        get
+        {
+            return pages.Length;
+        }
+    }
+    public Texture[] pageTexs;
+    public float rollSpeed = 1.0f;
+    public float rollCost = 2.0f;
+    public MeshRenderer[] pages;
+    public float dragSpeed = 10.0f;
+    public bool enableDrag = true;
+    public float dragPower = 1.0f;
+
+    private int curPage = 0;
+    private HashSet<int> rollingPages = new HashSet<int>();
+    private HashSet<int> rollingPages2 = new HashSet<int>();
+
+    private List<MeshRenderer> pagesList = new List<MeshRenderer>();
+    private bool isRolling = false;
+    List<int> left = new List<int>();
+    List<int> right = new List<int>();
+
+    void Start()
+    {
+        for (int i = 0; i < pageCount; i++)
+        {
+            var pageIdx = i - Mathf.FloorToInt(i / pageResCount) * pageResCount;
+            var page = pages[pageIdx];
+            pagesList.Add(page);
+        }
+        for (int i = 0; i < pageResCount; i++)
+        {
+            var idx = i;
+            var page = pages[idx];
+            var targetIdx = GetTextureIdx(idx);
+            PageData data = new PageData(pageTexs[2 * targetIdx], pageTexs[2 * targetIdx + 1], 180);
+            RefreshPage(page, data);
+            right.Add(idx);
+        }
+        FixPages();
+    }
+
+    private int GetTextureIdx(int idx)
+    {
+        return idx;
+    }
+
+    private void RefreshPage(MeshRenderer page, PageData data)
+    {
+        page.material.SetFloat("_Angle", data.angle);
+        page.material.SetTexture("_MainTex", data.main);
+        page.material.SetTexture("_SecondTex", data.second);
+    }
+
+
+    private void FixPages()
+    {
+        left.Sort();
+        right.Sort();
+        for (int i = 0; i < left.Count; i++)
+        {
+            var page = pagesList[left[i]];
+            page.material.renderQueue = 2000 + i;
+        }
+        for (int i = right.Count - 1; i >= 0; i--)
+        {
+            var page = pagesList[right[i]];
+            page.material.renderQueue = 2000 + right.Count - 1 - i;
+        }
+    }
+
+    private void MovePages(bool isRight = false)
+    {
+        var idx = isRight ? curPage - 2 : curPage + 1;
+        if (idx < 0 || idx >= pagesList.Count)
+            return;
+
+        var angle = isRight ? 0 : 180;
+        var page = pagesList[idx];
+        if (isRight)
+        {
+            if (page.Equals(pages[pages.Length - 1]))
+            {
+                if (right.Contains(idx))
+                    right.Remove(idx);
+                if (!left.Contains(idx))
+                    left.Add(idx);
+            }
+        }
+        else
+        {
+            if (page.Equals(pages[0]))
+            {
+                if (left.Contains(idx))
+                    left.Remove(idx);
+                if (!right.Contains(idx))
+                    right.Add(idx);
+            }
+        }
+        var targetIdx = GetTextureIdx(idx);
+        PageData data = new PageData(pageTexs[2 * targetIdx], pageTexs[2 * targetIdx + 1], angle);
+        RefreshPage(page, data);
+    }
+
+    private void LateUpdate()
+    {
+        List<int> discardRollingPages = new List<int>();
+        List<int> discardRollingPages2 = new List<int>();
+        foreach (var pageNum in rollingPages)
+        {
+            var page = pagesList[pageNum];
+            var angle = page.material.GetFloat("_Angle");
+            if (angle < 0.1)
+            {
+                page.material.SetFloat("_Angle", 0);
+                discardRollingPages.Add(pageNum);
+                if (!left.Contains(pageNum))
+                    left.Add(pageNum);
+                if (right.Contains(pageNum))
+                    right.Remove(pageNum);
+                curPage++;
+            }
+        }
+        foreach (var pageNum in rollingPages2)
+        {
+            var page = pagesList[pageNum];
+            var angle = page.material.GetFloat("_Angle");
+            if (Mathf.Abs(180 - angle) < 0.1)
+            {
+                page.material.SetFloat("_Angle", 180);
+                discardRollingPages2.Add(pageNum);
+                if (!right.Contains(pageNum))
+                    right.Add(pageNum);
+                if (left.Contains(pageNum))
+                    left.Remove(pageNum);
+                curPage--;
+            }
+        }
+        discardRollingPages.ForEach(pageNum =>
+        {
+            rollingPages.Remove(pageNum);
+        });
+        discardRollingPages2.ForEach(pageNum =>
+        {
+            rollingPages2.Remove(pageNum);
+        });
+
+        if (isRolling && !IsRolling())
+            FixPages();
+    }
+
+    void CheckInput()
+    {
+        if (!enableDrag)
+            return;
+        var input_x = Input.GetAxis("Mouse X");
+        if (Mathf.Abs(input_x)<dragPower)
+            return;
+        if (IsRolling())
+        {
+            int targetPageNum = -1;
+            foreach (var num in rollingPages)
+            {
+                targetPageNum = num;
+                rollingPages.Remove(curPage);
+                break;
+            }
+            if (targetPageNum < 0)
+            {
+                foreach (var num in rollingPages2)
+                {
+                    targetPageNum = num;
+                    rollingPages2.Remove(curPage);
+                    break;
+                }
+            }
+            if (targetPageNum < 0)
+            {
+                MeshRenderer page = pagesList[targetPageNum];
+            }
+        }
+  
+        //if (input_x > 0)
+        //{
+        //    RollRight();
+        //}
+        //else
+        //{
+        //    RollLeft();
+        //}
+    }
+
+    void Update()
+    {
+        CheckInput();
+
+        var delta = Time.deltaTime * rollSpeed;
+        foreach (var pageNum in rollingPages)
+        {
+            var page = pagesList[pageNum];
+            var angle = page.material.GetFloat("_Angle");
+            var targetAngle = Mathf.Lerp(angle, 0, delta / rollCost);
+            page.material.SetFloat("_Angle", targetAngle);
+        }
+        foreach (var pageNum in rollingPages2)
+        {
+            var page = pagesList[pageNum];
+            var angle = page.material.GetFloat("_Angle");
+            var targetAngle = Mathf.Lerp(angle, 180, delta / rollCost);
+            page.material.SetFloat("_Angle", targetAngle);
+        }
+        isRolling = IsRolling();
+    }
+
+    private bool IsRollingLeft()
+    {
+        return rollingPages.Count > 0;
+    }
+    private bool IsRollingRight()
+    {
+        return rollingPages2.Count > 0;
+    }
+    private bool IsRolling()
+    {
+        return IsRollingLeft() || IsRollingRight();
+    }
+
+    public void RollLeft()
+    {
+        if (IsRolling())
+            return;
+        if (curPage >= pageCount)//pages.Length)
+            return;
+        MovePages();
+        if (rollingPages2.Contains(curPage))
+        {
+            rollingPages2.Remove(curPage);
+        }
+        rollingPages.Add(curPage);
+        //curPage++;
+    }
+
+    public void RollRight()
+    {
+        if (IsRolling())
+            return;
+        if (curPage <= 0)
+            return;
+        MovePages(true);
+        if (rollingPages.Contains(curPage - 1))
+        {
+            rollingPages.Remove(curPage - 1);
+        }
+        rollingPages2.Add(curPage - 1);
+        //curPage--;
+    }
+
+}
+
+public struct PageData
+{
+    public Texture main;
+    public Texture second;
+    public float angle;
+
+
+    public PageData(Texture main, Texture second, float angle)
+    {
+        this.main = main;
+        this.second = second;
+        this.angle = angle;
+    }
+}
---
+++ Assets/Custom/PageRoll/fanshu.shader
@@ -0,0 +1,148 @@
+﻿Shader "Custom/PageRoll"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _SecondTex ("SecondTex", 2D) = "white" {}
+        _Angle("Angle",Range(0,180))= 180
+        _WaveLength("WaveLength",Range(1,10))=6.74
+
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            Cull Back
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _SecondTex;
+            float4 _SecondTex_ST;
+            float _Angle;
+            float _WaveLength;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                float s;
+                float c;
+                //通过该方法可以计算出该角度的正余弦值
+                sincos(radians(_Angle),s,c);
+                //旋转矩阵
+                float4x4 rotateMatrix={ 
+                    c ,s,0,0,
+                    -s,c,0,0,
+                    0 ,0,1,0,
+                    0 ,0,0,1
+                };
+                v.vertex -= float4(5,0,0,0);
+
+                float left = sin(v.vertex.x*_WaveLength) * s ;
+                float right = cos(v.vertex.x*_WaveLength) * (-c-1) ;
+                float factor = smoothstep(88,92,_Angle);
+                v.vertex.y = right*factor + (1-factor)*left;
+      
+                //顶点左乘以旋转矩阵
+                v.vertex = mul(rotateMatrix,v.vertex);
+                v.vertex += float4(5,0,0,0);
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _SecondTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_SecondTex, i.uv);
+
+                return col;
+            }
+            ENDCG
+        }
+        Pass
+        {
+            Cull Front
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _Angle;
+            float _WaveLength;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                float s;
+                float c;
+                //通过该方法可以计算出该角度的正余弦值
+                sincos(radians(_Angle),s,c);
+                //旋转矩阵
+                float4x4 rotateMatrix={ 
+                    c ,s,0,0,
+                    -s,c,0,0,
+                    0 ,0,1,0,
+                    0 ,0,0,1
+                };
+                v.vertex -= float4(5,0,0,0);
+
+                float left = sin(v.vertex.x*_WaveLength) * s ;
+                float right = cos(v.vertex.x*_WaveLength) * (-c-1) ;
+                float factor = smoothstep(88,92,_Angle);
+                v.vertex.y = right*factor + (1-factor)*left;
+        
+                //顶点左乘以旋转矩阵
+                v.vertex = mul(rotateMatrix,v.vertex);
+                v.vertex += float4(5,0,0,0);
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+    FallBack  "Transparent/Cutout/VertexLit"
+}
